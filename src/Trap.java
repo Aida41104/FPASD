ca86a142d2769090d598b3a5d259aa4766bf975a
Tic Tac Toe <namakuaidasafa@gmail.com>
2025-12-03 12:03:42 +0700

    +comment Trap.Java

--- src/Trap.java
+++ src/Trap.java
@@ -1,6 +1,9 @@
 import java.util.*;
 class Trap {
-    String question;
+    String question; //buat nyimpan pertanyaan
     String answer;
-    Trap(String q, String a){ question = q; answer = a; }
+    Trap(String q, String a){ //bikin trap (new Trap)
+        question = q; //nyimpen pertanyaan ke q, q disimpan di question
+        answer = a;
+    }
 }
