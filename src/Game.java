0416ea77a4222c68a9a4d8c3aa11bdc92c12cdee
Tic Tac Toe <namakuaidasafa@gmail.com>
2025-12-04 01:38:38 +0700

    [game] revisi + comment

--- src/Game.java
+++ src/Game.java
@@ -4,10 +4,10 @@ public class Game {
         Grid grid = new Grid(); //instantiates your game board.
         Player player = new Player(1,0); //spawn elmeen
         Monster monster = new Monster(9,9);
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //y/n
 
         while(true){
-            grid.render(player,monster);
+            grid.render(player,monster); //visual player n monster
 
             if(player.pos.equals(new Position(14,13))){ //koordinat finish
                 System.out.println("YOU WIN!");
@@ -18,10 +18,10 @@ public class Game {
                 break;
             }
 
-            grid.spawnTrap(player);
-            player.move(grid);
-            grid.checkTrap(player, sc);
-            monster.moveTowards(player, grid);
+            grid.spawnTrap(player); //spawn trap. misal Player di (5,5) Trap muncul di (5,8)
+            player.move(grid);//wasd gerak
+            grid.checkTrap(player, sc); //player nginjek trap ga?
+            monster.moveTowards(player, grid); // m grk 1 langkah
             System.out.println();
         }
     }
