7f851d7080a20a70332a5cd01b5a095e2e302b88
Tic Tac Toe <namakuaidasafa@gmail.com>
2025-12-03 11:53:09 +0700

    -bbrp position, +comment

--- src/Position.java
+++ src/Position.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 // Posisi x,y di grid
 class Position {
-    int x, y;
-    Position(int x, int y){ this.x = x; this.y = y; }
+    int x, y; //koordinat posisi objek
+    Position(int x, int y){ this.x = x; this.y = y; } //misal new Position(3,4)
 
-    public boolean equals(Object obj){
-        if(this == obj) return true;
-        if(obj == null || getClass() != obj.getClass()) return false;
-        Position p = (Position)obj;
-        return x == p.x && y == p.y;
+    public boolean equals(Object obj){ //buat bandingin 2 posisi iut sama apa ga
+        Position p = (Position)obj; //cm ngubah objek jadi position, biar bisa tau p.x
+        return x == p.x && y == p.y; // true klo koordinat sama
     }
 
     public int hashCode(){
-        return Objects.hash(x,y);
+        return Objects.hash(x,y); //ngasih id(?) untuk setiap koordinat biar beliaunya ga nyari lama-lama
     }
 }
