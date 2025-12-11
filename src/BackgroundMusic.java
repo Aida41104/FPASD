9ef08a7fee2be5d9515517b5cd26fc8b497cc041
Candleline <candlelinef@gmail.com>
2025-12-11 09:29:05 +0700

    Add BackgroundMusic class to play audio

---
+++ src/BackgroundMusic.java
@@ -0,0 +1,22 @@
+import javax.sound.sampled.*;
+import java.io.File;
+
+public class BackgroundMusic {
+
+    public BackgroundMusic() {
+        try {
+            File soundFile = new File("src/sounds/bgm.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+
+            System.out.println("Background music started.");
+
+        } catch (Exception e) {
+            System.out.println("Failed to play background music: " + e.getMessage());
+        }
+    }
+}
